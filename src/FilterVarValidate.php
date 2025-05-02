d24609053473abcc7b0a36d90b5b1c55de28f5af
Doug Wilbourne <dougwilbourne@gmail.com>
2025-05-02 10:04:40 -0400

    added testValue method to FilterVarValidate.php so it can implement ValTesterInterface

--- src/FilterVarValidate.php
+++ src/FilterVarValidate.php
@@ -11,6 +11,8 @@ use pvc\interfaces\filtervar\FilterVarValidateInterface;
 
 /**
  * Class FilterVarValidate
+ * FilterVarValidateInterface extends ValtesterInterface so this object is also
+ * a ValTester object
  */
 class FilterVarValidate extends FilterVar implements FilterVarValidateInterface
 {
@@ -22,5 +24,10 @@ class FilterVarValidate extends FilterVar implements FilterVarValidateInterface
     public function validate(string $value): bool
     {
         return (false !== filter_var($value, $this->getFilter(), $this->getOptionsFlagsArray()));
+    }
+
+    public function testValue(string $value): bool
+    {
+        return $this->validate($value);
     }
 }
